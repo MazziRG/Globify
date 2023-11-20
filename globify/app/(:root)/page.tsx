2dc36bc89223dbee158f9db75962561f12e818e9
Mazzi Garcia <mazzi@Mazzis-MBP-2.hsd1.fl.comcast.net>
2023-11-19 22:49:33 -0500

    Set up

---
+++ globify/app/(:root)/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-bold text-teal-500 text-2xl">Home Page</h1>
+    </main>
+  );
+}
